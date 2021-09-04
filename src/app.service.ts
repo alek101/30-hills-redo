534eec6e16e505064a8e6ef4fa6cc01fa9e64cb3
alek101 <47416711+alek101@users.noreply.github.com>
2021-09-04 17:49:28 +0200

    finished documentation
    added global prefix

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,19 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return `<h1>Documentation</h1> <ul>
+     <li>
+        <h4>List of direct friends</h4>
+        <p> /api/v1/people/direct/:id </p>
+      </li> 
+      <li>
+        <h4>List of friends of friends</h4>
+        <p> /api/v1/people/friends/:id </p>
+      </li> 
+      <li>
+        <h4>List of suggested friends</h4>
+        <p> /api/v1/people/suggested/:id </p>
+      </li>  
+     </ul>`;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import { RequestMethod } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v1/', {
+    exclude: [{ path: '/', method: RequestMethod.GET }],
+  });
   await app.listen(3000);
 }
 bootstrap();
